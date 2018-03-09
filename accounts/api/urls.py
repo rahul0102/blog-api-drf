4c745e223c389c4aff08942fb219280bc2ca6626
rahul_raval <rahulraval1997@gmail.com>
2018-03-09 19:35:20 +0530

    Added swagger UI

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
+
+from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
+
 from .views import (
     UserViewSet
 )
 app_name = 'accounts-api'
+
+
+schema_view = get_schema_view(title='Users API', renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer])
 
 #######
 # Using Router for viewsets
@@ -11,6 +18,7 @@ app_name = 'accounts-api'
 router = DefaultRouter()
 router.register('users', UserViewSet, 'users')
 urlpatterns = [
+    url(r'^swagger/', schema_view, name="docs"),
     url(r'^', include(router.urls)),
 ]
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
